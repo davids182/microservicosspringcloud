6e2d29c34e2fd8a7f7a6dfcea745b3ad0d4bf82d
David de Oliveira <88116507+davids182@users.noreply.github.com>
2021-08-28 23:03:40 -0300

    Add files via upload

---
+++ spring-cloud-dio/service-discovery/src/main/java/com/ServiceDiscoveryApplication.java
@@ -0,0 +1,14 @@
+package com.servicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceDiscoveryApplication.class, args);
+	}
+
+}
